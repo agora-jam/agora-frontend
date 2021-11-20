8eeb43cec701fbb93f90643ce59b7b6c463b0127
Ray KananI <hello@raykanani.com>
2021-11-20 15:42:55 -0500

    splash page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../../router/routeList';
 
 const Home = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="text-center hero-content">
+    <div
+      className="hero min-h-screen bg-base-200"
+      style={{
+        // eslint-disable-next-line no-undef
+        backgroundImage: `url(${require('../../../assets/images/bg-home.jpeg')})`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-80"></div>
+      <div className="text-center hero-content text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">AGORA</h1>
-          <p className="mb-5">The first film festival dapp.</p>
-          <button className="btn btn-primary">Tokenize My Movie</button>
+          <h1 className="mb-2 text-6xl font-bold">AGORA</h1>
+          <p className="mb-5 text-2xl">The first decentralized film festival.</p>
+          <Link className="btn btn-primary mb-4 " to={routes.createFilm}>
+            Create My NFT Film Submission
+          </Link>
+          <Link className="btn btn-accent" to={routes.createFilm}>
+            View Films Submissions
+          </Link>
         </div>
       </div>
     </div>
