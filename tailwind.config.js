1ba585009add8406cdaade7c501284cbd0d5b618
Ray KananI <hello@raykanani.com>
2021-11-20 00:03:40 -0500

    add Tailwind Forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // purge: ['./src/**/*.html', './src/**/*.js'],
+  purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +7,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'), '@tailwindcss/forms'],
 };
