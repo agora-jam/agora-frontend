d310ddd27849cf2476199f3fa7b238a6188c7a0a
Diego Coronel <diegocoronel1997@gmail.com>
2021-11-21 11:21:40 -0500

    filmList functionality complete

--- src/views/FilmList/FilmList.jsx
+++ src/views/FilmList/FilmList.jsx
@@ -1,65 +1,76 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { useEffect, useState } from 'react';
-// import useStore from '../../store/index.js';
-// import axios from 'axios';
-// import { ethers } from 'ethers';
-// import Agora from '../../contracts/agora.json';
-// import { agoraAddress } from '../../../config.js';
+import { useEffect, useState } from 'react';
+import useStore from '../../store/index.js';
+import axios from 'axios';
+import { ethers } from 'ethers';
+import Agora from '../../contracts/agora.json';
+import AgoraShare from '../../contracts.agoraShare.json';
+
+import { agoraAddress, agoraShareAddress } from '../../../config.js';
 
 import mockData from './mockData';
 
 const FilmList = () => {
-  // const { genericProvider } = useStore((state) => state);
-  // const [nfts, setNfts] = useState([]);
-  // const [loadingState, setLoadingState] = useState('not-loaded');
+  const { genericProvider } = useStore((state) => state);
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState('not-loaded');
 
-  // useEffect(() => {
-  //   loadNFTs();
-  // }, []);
+  useEffect(() => {
+    loadNFTs();
+  }, []);
 
-  // const loadNFTs = async () => {
-  //   const agoraContract = new ethers.Contract(agoraAddress, Agora.abi, genericProvider);
+  const loadNFTs = async () => {
+    const agoraContract = new ethers.Contract(agoraAddress, Agora.abi, genericProvider);
+    const agoraShareContract = new ethers.Contract(
+      agoraShareAddress,
+      AgoraShare.abi,
+      genericProvider,
+    );
 
-  //   const data = await agoraContract.getAllMovies();
+    const data = await agoraContract.getAllMovies();
 
-  //   const items = await Promise.all(
-  //     data.map(async (i) => {
-  //       const tokenUrl = await agoraContract.getOneMovie(i.tokenId);
-  //       const meta = await axios.get(tokenUrl);
-  //       const item = {
-  //         tokenId: i.tokenId.toNumber(),
-  //         video: meta.data.video,
-  //         image: meta.data.image,
-  //         name: meta.data.name,
-  //         description: meta.data.description,
-  //         genre: meta.data.genre,
-  //         language: meta.data.language,
-  //         country: meta.data.country,
-  //         duration: meta.data.duration,
-  //         imageUrl: meta.data.imageUrl,
-  //         videoUrl: meta.data.videoUrl,
-  //         fundraisingAmount: meta.data.fundraisingAmount,
-  //         numberOfTokens: meta.data.numberOfTokens,
-  //         percentageGiven: meta.data.percentageGiven,
-  //       };
-  //       return item;
-  //     }),
-  //   );
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUrl = await agoraContract.getOneMovie(i.tokenId);
+        const meta = await axios.get(tokenUrl);
+        const amountRaised = await agoraShareContract.getSharedDropStruct(i.tokenId);
+        const item = {
+          tokenId: i.tokenId.toNumber(),
+          amountRaised,
+          video: meta.data.video,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+          genre: meta.data.genre,
+          language: meta.data.language,
+          country: meta.data.country,
+          duration: meta.data.duration,
+          imageUrl: meta.data.imageUrl,
+          videoUrl: meta.data.videoUrl,
+          fundraisingAmount: meta.data.fundraisingAmount,
+          numberOfTokens: meta.data.numberOfTokens,
+          percentageGiven: meta.data.percentageGiven,
+        };
+        return item;
+      }),
+    );
 
-  //   setNfts(items);
-  //   setLoadingState('loaded');
-  // };
+    setNfts(items);
+    setLoadingState('loaded');
+  };
 
-  // if (loadingState === 'not-loaded') return <div>Loading...</div>;
+  if (loadingState === 'not-loaded') return <div>Loading...</div>;
 
   return (
     <div>
-      {mockData.map((item, i) => {
+      {nfts.map((item, i) => {
         return (
           <div key={i}>
             <Link to={{ pathname: `film/${item.tokenId}`, state: item }}>
-              <div>{item.name}</div>
+              <div>
+                {item.name} {item.description}
+              </div>
             </Link>
           </div>
         );
--- src/views/FilmPage/FilmPage.jsx
+++ src/views/FilmPage/FilmPage.jsx
@@ -1,67 +1,66 @@
 import React from 'react';
-// import { useParams } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
-// import Web3Modal from 'web3modal';
-// import ethers from 'ethers';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Web3Modal from 'web3modal';
+import ethers from 'ethers';
 
-// import { useEffect, useState } from 'react';
-// import useStore from '../../store/index.js';
-// import axios from 'axios';
-// import { ethers } from 'ethers';
-// import Agora from '../../contracts/agora.json';
-// import { agoraAddress } from '../../../config.js';
+import { useEffect, useState } from 'react';
+import useStore from '../../store/index.js';
+import axios from 'axios';
+import Agora from '../../contracts/agora.json';
+import { agoraAddress } from '../../../config.js';
 
 const FilmPage = () => {
-  // const { filmId } = useParams();
-  // // const { genericProvider } = useStore((state) => state);
-  // const [movie, setMovie] = useState(location.state);
-  // const [filmStatus, setFilmStatus] = useState('active');
+  const { filmId } = useParams();
+  // const { genericProvider } = useStore((state) => state);
+  const [movie, setMovie] = useState(location.state);
+  const [filmStatus, setFilmStatus] = useState('active');
 
-  // const nftBreakdown = [
-  //   'Deleted scenes NFT',
-  //   'Script NFT',
-  //   'Name on Credits',
-  //   'Buyout payback',
-  //   'Revenue share',
-  // ];
+  const nftBreakdown = [
+    'Deleted scenes NFT',
+    'Script NFT',
+    'Name on Credits',
+    'Buyout payback',
+    'Revenue share',
+  ];
 
-  // const buyNft = async (nft) => {
-  //   const web3Modal = new Web3Modal();
-  //   const connection = await web3Modal.connect();
-  //   const provider = new ethers.providers.Web3Provider(connection);
-  //   const signer = provider.getSigner();
-  // };
+  const buyNft = async (nft) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+  };
 
-  // useEffect(() => {
-  //   getMovieDetails(filmId);
-  // }, []);
+  useEffect(() => {
+    getMovieDetails(filmId);
+  }, []);
 
-  // const getMovieDetails = async (id) => {
-  //   const agoraContract = new ethers.Contract(agoraAddress, Agora.abi, genericProvider);
+  const getMovieDetails = async (id) => {
+    const agoraContract = new ethers.Contract(agoraAddress, Agora.abi, genericProvider);
 
-  //   const tokenUrl = await agoraContract.getOneMovie(id);
-  //   const meta = await axios.get(tokenUrl);
-  //   setMovie({
-  //     tokenId: id.toNumber(),
-  //     video: meta.data.video,
-  //     image: meta.data.image,
-  //     name: meta.data.name,
-  //     description: meta.data.description,
-  //     genre: meta.data.genre,
-  //     language: meta.data.language,
-  //     country: meta.data.country,
-  //     duration: meta.data.duration,
-  //     imageUrl: meta.data.imageUrl,
-  //     videoUrl: meta.data.videoUrl,
-  //     fundraisingAmount: meta.data.fundraisingAmount,
-  //     numberOfTokens: meta.data.numberOfTokens,
-  //     percentageGiven: meta.data.percentageGiven,
-  //   });
-  // };
+    const tokenUrl = await agoraContract.getOneMovie(id);
+    const meta = await axios.get(tokenUrl);
+    setMovie({
+      tokenId: id.toNumber(),
+      video: meta.data.video,
+      image: meta.data.image,
+      name: meta.data.name,
+      description: meta.data.description,
+      genre: meta.data.genre,
+      language: meta.data.language,
+      country: meta.data.country,
+      duration: meta.data.duration,
+      imageUrl: meta.data.imageUrl,
+      videoUrl: meta.data.videoUrl,
+      fundraisingAmount: meta.data.fundraisingAmount,
+      numberOfTokens: meta.data.numberOfTokens,
+      percentageGiven: meta.data.percentageGiven,
+    });
+  };
 
   return (
     <div>
-      {/* <h1>{movie.name}</h1>
+      <h1>{movie.name}</h1>
       <Link to={`/watch/${movie.tokenId}`}>
         <button
           className="btn btn-primary btn-active rounded-none	"
@@ -78,7 +77,7 @@ const FilmPage = () => {
             return <div key={i}>{item}</div>;
           })}
         </div>
-      )} */}
+      )}
     </div>
   );
 };
